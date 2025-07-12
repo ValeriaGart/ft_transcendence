7711561009e182a28a60b11870dba05b8a7ce68d
Luca <ca.lu39@yahoo.de>
2025-07-12 18:30:31 +0000

    add cookie auth

--- backend/routes/websocket.routes.js
+++ backend/routes/websocket.routes.js
@@ -2,7 +2,11 @@ import WebsocketController from '../controllers/websocket.controller.js'
 
 async function routes(fastify, options) {
 	const controller = new WebsocketController(fastify.websocketServer);
-	fastify.get('/hello-ws', { websocket: true }, (connection, req) => {
+
+	fastify.get('/hello-ws', { 
+		websocket: true,
+		preHandler: [fastify.authenticate]
+		}, (connection, req) => {
 		controller.handleConnection(connection);
 	});
 }

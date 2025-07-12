8d4bb16b91143fb75ed948f1712593eb66e1f551
Luca <ca.lu39@yahoo.de>
2025-07-12 20:04:55 +0000

    add onlineFriends function

--- backend/services/websocket.service.js
+++ backend/services/websocket.service.js
@@ -1,3 +1,6 @@
+import { dbGet } from "../config/database.js";
+import FriendService from "./friend.service.js";
+
 class WebsocketService {
     constructor(websocketServer) {
         this.websocketServer = websocketServer;
@@ -6,22 +9,23 @@ class WebsocketService {
 /* <><><><><><><><><><><><><><><><><><><><><><><><> */
 
 	handleJoin(connection, wsid) {
+		connection.userId = wsid;
 		this.broadcast({
 			sender: '__server',
 			message: `id ${wsid} joined`
 		}, connection);
 	}
 
-	handleLeave(connection, wsid) {
+	handleLeave(connection) {
 		connection.on('close', () => {
 			this.broadcast({
 				sender: '__server',
-				message: `id ${wsid} left`
+				message: `id ${connection.userId} left`
 			});
 		});
 	}
 
-	handleMessage(connection, wsid) {
+	handleMessage(connection) {
 		connection.on('message', message => {
 			try {
 				const parsedMessage = JSON.parse(message);
@@ -32,12 +36,13 @@ class WebsocketService {
 				if (parsedMessage.type === 1) {
 					console.log("[handleMessage] type 1")
 					this.broadcast({
-						sender: `${wsid}`,
+						sender: `${connection.userId}`,
 						message: `${parsedMessage.message}`
 					}, connection);
 				}
 				else if (parsedMessage.type === 2) {
 					console.log("[handleMessage] type 2");
+					this.onlineFriends(connection);
 				}
 				else {
 					console.log("[handleMessage] unknown type");
@@ -50,9 +55,34 @@ class WebsocketService {
 
 /* <><><><><><><><><><><><><><><><><><><><><><><><> */
 
+	async onlineFriends(connection) {
+		const onlineFriends = [];
+		for (let client of this.websocketServer.clients) {
+			if (connection.userId === client.userId) {
+				continue ;
+			}
+			try {
+				const friend = await FriendService.getFriendshipStatus(connection.userId, client.userId);
+				if (friend === 1) {
+					// add to list of friends who are online
+					onlineFriends.push({
+						userId: client.userId
+					});
+					// console.log("1)", connection.userId, " ", client.userId, " are friends ", friend);
+				}
+				// console.log("2)", connection.userId, " ", client.userId, friend);
+			} catch (error) {
+				console.log("db error probably: ", error.message);
+			}
+			// console.log("3)", connection.userId, " ", client.userId, onlineFriends);
+		}
+		connection.send(JSON.stringify({ onlineFriends }));
+	}
+
 	broadcast(message, excludeConnection = null) {
         for (let client of this.websocketServer.clients) {
-            if (client.readyState === 1 && client !== excludeConnection) {
+			if (client.readyState === 1 && client !== excludeConnection) {
+				console.log("client ", client.userId);
                 client.send(JSON.stringify(message));
             }
         }

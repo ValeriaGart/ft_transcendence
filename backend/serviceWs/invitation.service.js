8d3060896534c61f0495110b4a27a3fa3739938e
Luca <ca.lu39@yahoo.de>
2025-07-20 10:45:53 +0000

    refactoring into InvitationService

---
+++ backend/serviceWs/invitation.service.js
@@ -0,0 +1,20 @@
+
+class InvitationService {
+		
+	static async sendInvitation() {
+		console.log("[InvitationService] sending invitations");
+		return ;
+	}
+
+
+	
+	static async matchMakingAcceptInvitation() {
+		//👉 set status to accepted for player
+		//👉 check if all accepted
+		// 👉	if yes, then startMatch()
+		// }
+	}
+
+}
+
+export default InvitationService;

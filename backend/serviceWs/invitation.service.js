d7fc0da6218929d4fc0346865b6cf9c9ccc8806b
Luca <ca.lu39@yahoo.de>
2025-07-20 13:10:46 +0000

    sending invitations

--- backend/serviceWs/invitation.service.js
+++ backend/serviceWs/invitation.service.js
@@ -1,8 +1,37 @@
+import RoomUtilsService from "./roomutils.service.js";
 
 class InvitationService {
-		
-	static async sendInvitation() {
+
+	static async createInvitationMessage(room) {
+		const sanitizedPlayers = room.players.map(player => {
+			const { wsclient, ...rest } = player; // Exclude wsclient
+			return rest;
+		});
+		const message = {
+			sender: "__server",
+			message: "You were invited to this room.",
+			roomId: room.id,
+			players: sanitizedPlayers,
+			gameMode: room.gameMode,
+			oppMode: room.oppMode
+		}
+		return message;
+	}
+
+	static async sendInvitation(websocketService, room) {
 		console.log("[InvitationService] sending invitations");
+
+		const pendingPlayers = RoomUtilsService.getAllPendingPlayerNicksRoom(room);
+		for (let p of room.players) {
+			if ((await pendingPlayers).includes(p.nick)) {
+				if (!p.wsclient) {
+					console.log("[RoomUtilsService] can't send invitation to client without websocket connection");
+					/* 👉 todo: throw error */
+					break ;
+				}
+				websocketService.sendMessageToClient(p.wsclient, await this.createInvitationMessage(room));
+			}
+		}
 		return ;
 	}
 

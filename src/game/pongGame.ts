5c25056fdf0fa402c6fcae6bbebd5924d0bda913
moth <katzenbeisserjohannes@gmail.com>
2025-08-23 22:09:02 +0200

    properly send msg5 at end of offline matches

--- src/game/pongGame.ts
+++ src/game/pongGame.ts
@@ -192,6 +192,15 @@ export class PongGame {
 			console.log('Sending finish & save match msg:', JSON.stringify(msg));
 			this._engine._ws.sendMessage(JSON.stringify(msg));
 		}
+		else if (this._oppMode != OpponentMode.ONLINE) {
+			const msg = {
+				"type": 5,
+				"roomId": this._engine._roomID,
+				"status": "finished"
+			}
+			console.log('Sending finish match msg:', JSON.stringify(msg));
+			this._engine._ws.sendMessage(JSON.stringify(msg));
+		}
 	}
 
 	private checkWinCondition(): boolean {
